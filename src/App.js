5890d0f2319544ddc8d5aa6e9a3a3be24d9245a2
GustPhitchaya <77724168+GustPhitchaya@users.noreply.github.com>
2022-06-09 14:14:25 +0700

    created list structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
+function AddButton(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className='add-button' onClick={props.onClick}>
+      add new item
+    </button>
   );
+}
+
+class Item extends React.Component {
+  render() {
+    const id = this.props.id;
+    const desc = this.props.desc;
+
+    return (
+      <>
+      <input type='checkbox' id={id} value={desc}/>
+      <label htmlFor={id}>{desc}</label>
+      </>
+    );
+  }
+}
+
+class List extends React.Component {  
+  addNewItem() {
+
+  }
+
+  renderAddButton() {
+    return (
+      <AddButton 
+        onClick={() => this.addNewItem()} 
+      />
+    );
+  }
+
+  renderItem(item) {
+    return (
+      <Item 
+        id={item.id}
+        type={item.type}
+        desc={item.desc}
+      />
+    );
+  }
+
+  renderList() {
+    const items = getItems();
+    return items.map(item => {
+      return <li className='Item'>
+        {this.renderItem(item)}
+      </li>
+    });
+  }
+
+  render() {
+    return (
+      <ul className='List'>
+        {this.renderList()}
+        {this.renderAddButton()}
+      </ul>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <p>To-Do List</p>
+        </header>
+        <div className="Todo-list">
+          <List />
+        </div>
+      </div>
+    );
+  }
+}
+
+function getItems() {
+  //example
+  return [
+    {
+      id: 'i1',
+      type: 'study',
+      desc: 'homework'
+    },
+    {
+      id: 'i2',
+      type: 'work',
+      desc: 'web'
+    }
+  ];
 }
 
 export default App;
