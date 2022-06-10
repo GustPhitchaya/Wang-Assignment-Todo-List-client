0e9a85226ecea7d15d1eb6e94e3e0922968007d1
GustPhitchaya <77724168+GustPhitchaya@users.noreply.github.com>
2022-06-10 15:14:52 +0700

    able to fetch dat from api

--- src/App.js
+++ src/App.js
@@ -3,9 +3,25 @@ import "./App.css";
 import List from "./List.js";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: null };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/mongo")
+      .then(res => res.json())
+      .then(items => this.setState({ items: items.name }));
+  }
+
+  UNSAFE_componentWillMount() {
+    this.callAPI();
+  }
+
   render() {
     return (
       <div className="App" >
+        <p>{this.state.items}</p>
         <header className="App-header">
           <p>To-Do List</p>
         </header>
